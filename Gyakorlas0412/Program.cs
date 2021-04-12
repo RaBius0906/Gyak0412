ee18d24f0acb776bb3e85e42256f3951d0a7cf22
RáczBianka <bianka.racz@gmail.com>
2021-04-12 17:57:27 +0200

    adatok_szama

--- Gyakorlas0412/Program.cs
+++ Gyakorlas0412/Program.cs
@@ -16,6 +16,9 @@ namespace Gyakorlas0412
             File.ReadAllLines("forras.csv"); //bin mappában kell lennie ennek a fájlban
 
             Console.WriteLine($"Adatok száma");
+            int N = sorok.Length; // ha van fejléc akkor -1, mert azt le kell vonni
+
+
             Console.WriteLine($"Van-e valamilyen?");
 
 
